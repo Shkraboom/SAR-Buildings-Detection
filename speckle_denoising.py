e06a2460bb58f575fa8f55eac4e75e271ae0d5f2
Shkraboom <110823433+Shkraboom@users.noreply.github.com>
2024-01-31 13:42:38 +0500

    Add files via upload

---
+++ speckle_denoising.py
@@ -0,0 +1,12 @@
+import cv2
+
+radar_image = cv2.imread('input_image', cv2.IMREAD_GRAYSCALE)
+
+window_size = 20
+
+filtered_image = cv2.fastNlMeansDenoising(radar_image, None, h=window_size)
+
+cv2.imshow('Original Image', radar_image)
+cv2.imshow('Filtered Image', filtered_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
