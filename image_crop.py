107937e99705ef7e60de75171f3be1cbc53ea545
Shkraboom <shkraboom@gmail.com>
2024-03-10 01:43:53 +0300

    -

--- image_crop.py
+++ image_crop.py
@@ -2,12 +2,14 @@ from PIL import Image
 import numpy as np
 import os
 
-def split_and_save_image(image_path, output_folder):
+image_path = "path_to_input_image"
+output_folder = "path_to_output_folder"
+chunk_size = 512
+
+def split_and_save_image(image_path, output_folder, chunk_size):
     img = Image.open(image_path)
 
     width, height = img.size
-
-    chunk_size = 512
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -24,7 +26,4 @@ def split_and_save_image(image_path, output_folder):
             chunk_filename = f"{output_folder}/chunk_{x}_{y}.jpg"
             Image.fromarray(chunk_array).save(chunk_filename)
 
-image_path = "path_to_input_image"
-output_folder = "path_to_output_folder"
 
-split_and_save_image(image_path, output_folder)
