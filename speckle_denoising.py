ee063a2eff819483acaa0d48880af57e6c22edda
Shkraboom <shkraboom@gmail.com>
2024-03-10 01:49:44 +0300

    -

--- speckle_denoising.py
+++ speckle_denoising.py
@@ -1,11 +1,16 @@
 import cv2
 
-radar_image = cv2.imread('input_image', cv2.IMREAD_GRAYSCALE)
-
+image_path = "path_to_input_image"
+output_folder = "path_to_output_folder"
 window_size = 20
+def filter(image_path, output_folder, window_size):
+    radar_image = cv2.imread('path_to_input_image')
 
-filtered_image = cv2.fastNlMeansDenoising(radar_image, None, h=window_size)
+    filtered_image = cv2.fastNlMeansDenoising(radar_image, None, h=window_size)
 
-cv2.imshow('Original Image', radar_image)
-cv2.imshow('Filtered Image', filtered_image)
-cv2.waitKey(0)
+    #cv2.imshow('Original Image', radar_image)
+    #cv2.imshow('Filtered Image', filtered_image)
+    #cv2.waitKey(0)
+
+    output_path = output_folder + "/filtered_image.jpg"
+    cv2.imwrite(output_path, filtered_image)
